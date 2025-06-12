4845dd906f1382734ab4c3791a9d137a9f626d37
AAlvarez-Ortega <alvarez.ortega.aldouriel@gmail.com>
2025-06-12 01:06:03 -0600

    11

---
+++ JS/footer.js
@@ -0,0 +1,8 @@
+// JS/footer.js
+const footer = document.createElement("footer");
+footer.innerHTML = `
+  <div class="footer-container">
+    <p>&copy; 2025 OhanaShop. Todos los derechos reservados.</p>
+  </div>
+`;
+document.body.appendChild(footer);
